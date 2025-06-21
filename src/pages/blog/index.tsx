8c4cfccce6e2b4ab29f838d75d646c82288298eb
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-06-21 01:51:25 -0300

    v0: landing page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,6 @@
-import { Header } from "../../components/header";
-
 export default function BlogPage() {
   return (
     <>
-      <Header />
       <h2 className="text-black">Blog</h2>
     </>
   );
